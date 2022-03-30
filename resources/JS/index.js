254d3e57c3c04065f04c0f38366cf76bc15770fe
Brennan Emerson <brennanemerson15@gmail.com>
2022-03-29 19:16:03 -0700

    Added more supprt for different screens
    Made the profile pic adjust itself to different screen sizzes

---
+++ resources/JS/index.js
@@ -0,0 +1,6 @@
+let profilePicture = document.getElementById('myimage');
+
+function profileStyling(element){
+    document.element.style.padding = "0 20px 20px 0"
+}
+profilePicture.addEventListener('mouseover', profileStyling )
