c8c28a61ed291622e7df1526b9cf569fba832c73
Brennan Emerson <brennanemerson15@gmail.com>
2022-04-02 12:23:54 -0700

    New Portfolio website
    Changed the structure of the website, background color, and gave it a more modern look.

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -1,6 +1,1 @@
-let profilePicture = document.getElementById('myimage');
-
-function profileStyling(element){
-    document.element.style.padding = "0 20px 20px 0"
-}
-profilePicture.addEventListener('mouseover', profileStyling )
+let icon = document.getElementById()
